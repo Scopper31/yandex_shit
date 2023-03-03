cdc30a10ab0598400843f98e6520c1f02682436a
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-03 19:46:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import PySimpleGUI as sg
 import main5
 
-layout = [[sg.Text('your yandex login:         '), sg.InputText(size=(50,1))],
-          [sg.Text('your yandex password:  '), sg.InputText(size=(50,1))],
-          [sg.Text('link of the lesson:          '), sg.InputText(size=(50,1))],
-          [sg.Text('number of the task or -1:'), sg.InputText(size=(50,1))],
+layout = [[sg.Text('your yandex login:', size=(15, None)), sg.InputText(size=(50, 1))],
+          [sg.Text('your yandex password:', size=(15, None)), sg.InputText(size=(50, 1))],
+          [sg.Text('link of the lesson:', size=(15, None)), sg.InputText(size=(50, 1))],
+          [sg.Text('number of the task or -1:', size=(15, None)), sg.InputText(size=(50, 1))],
           [sg.Button('Start'), sg.Button('Stop')]]
 
 window = sg.Window('Yandex Lyceum', layout)
@@ -17,7 +17,7 @@ while True:
 
     if event == 'Start':
         main5.username = values[0]
-        main5.password= values[1]
+        main5.password = values[1]
         main5.lesson_url = values[2]
         main5.one_task = int(values[3])
         main5.main()
