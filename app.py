b0e01c9b9aad2717584a089d04650465fc14ff81
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-03 17:59:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+import main5
+
+layout = [[sg.Text('your yandex login:         '), sg.InputText(size=(50,1))],
+          [sg.Text('your yandex password:  '), sg.InputText(size=(50,1))],
+          [sg.Text('link of the lesson:          '), sg.InputText(size=(50,1))],
+          [sg.Text('number of the task or -1:'), sg.InputText(size=(50,1))],
+          [sg.Button('Start'), sg.Button('Stop')]]
+
+window = sg.Window('Yandex Lyceum', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED or event == 'Stop':
+        break
+
+    if event == 'Start':
+        main5.username = values[0]
+        main5.password= values[1]
+        main5.lesson_url = values[2]
+        main5.one_task = int(values[3])
+        main5.main()
+window.close()
