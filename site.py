ffd29cba609d229810622062c753c9229974cb8a
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-05 13:59:13 +0300

    Update site.py

--- site.py
+++ site.py
@@ -1,23 +1,22 @@
 from flask import Flask, request
 import json
 
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def check_params():
-    log = request.form['login']
-    t_n = request.form['time_now']
-    with open('data.json') as json_file:
-        data = json.load(json_file)
-        if log in data:
-            if t_n < data[log]:
-                return 'True'
-            else:
-                return 'False'
-        else:
-            return 'False'
-
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    view = ''
+    if request.method == 'POST':
+        log = request.form['login']
+        e_t = request.form['ending_time']
+        with open('data.txt', 'w') as file:
+            file.write(log + ':' + e_t)
+    with open('data.txt') as file:
+        li = file.readlines()
+        view += '<br/>'.join(li)
+    return view
 
 
 if __name__ == '__main__':
