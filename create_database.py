f2d15e8cf9a574b7aaff3d30fa44d77eac4cbbd0
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-14 23:20:44 +0300

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -13,7 +13,7 @@ def sql_connection():
 
 # def sql_table(con):
 #     sqlite_create_table_query = '''CREATE TABLE sqlitedb_subscribers (
-#                                         email text NOT NULL UNIQUE,
+#                                         email TEXT NOT NULL UNIQUE,
 #                                         ending_date timestamp);'''
 #     cursor = con.cursor()
 #     cursor.execute(sqlite_create_table_query)
@@ -41,18 +41,42 @@ def sql_connection():
 
 
 # def add_subscriber(con, email):
-#     time_now = datetime.now().date()
+#     time_now = datetime.now()
 #     half_of_year = timedelta(days=182)
 #     ending_time = time_now + half_of_year
 #     print(ending_time)
-#     sql_insert_query = f"""INSERT INTO sqlitedb_subscribers VALUES ('{email}', {ending_time})"""
+#     sql_insert_query = """INSERT INTO 'sqlitedb_subscribers'
+#                               ('email', 'ending_date')
+#                               VALUES (?, ?);"""
 #     cursor = con.cursor()
-#     cursor.execute(sql_insert_query)
+#     cursor.execute(sql_insert_query, (email, ending_time))
 #     con.commit()
 #     cursor.close()
+#
+#
+# sqlite_connection = sql_connection()
+# mail = input()
+# add_subscriber(sqlite_connection, mail)
+# sqlite_connection.close()
+
+
+# def check_subscribtion(con, mail):
+#     time_now = datetime.now()
+#     sqlite_select_query = """SELECT * from sqlitedb_subscribers where email = ?"""
+#     cursor = con.cursor()
+#     cursor.execute(sqlite_select_query, (mail,))
+#     record = cursor.fetchone()
+#     time = datetime.strptime(record[1], '%m/%d/%y %H:%M:%S')
+#     if time > time_now:
+#         con.commit()
+#         cursor.close()
+#         return True
+#     con.commit()
+#     cursor.close()
+#     return False
 # 
 # 
 # sqlite_connection = sql_connection()
 # mail = input()
-# add_subscriber(sqlite_connection, mail)
+# print(check_subscribtion(sqlite_connection, mail))
 # sqlite_connection.close()
