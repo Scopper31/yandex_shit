1475c7e17ec6a287d3cd100dcef79dd1695a7151
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-16 16:34:55 +0300

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -26,8 +26,24 @@ def sql_connection():
 # sqlite_connection.close()
 
 
+# def delete_pidor(con, email):
+#     time_now = datetime.now().date()
+#     sql_delete_query = f"""DELETE from sqlitedb_subscribers where email == {email}"""
+#     cursor = con.cursor()
+#     cursor.execute(sql_delete_query)
+#     con.commit()
+#     cursor.close()
+#
+#
+# pidors_email = input()
+# sqlite_connection = sql_connection()
+# delete_pidor(sqlite_connection, pidors_email)
+# sqlite_connection.close()
+
+
+
 # def delete_record(con):
-#     time_now = datetime.now().date().isoformat()
+#     time_now = datetime.now().date()
 #     sql_delete_query = f"""DELETE from sqlitedb_subscribers where ending_date < {time_now}"""
 #     cursor = con.cursor()
 #     cursor.execute(sql_delete_query)
@@ -41,17 +57,19 @@ def sql_connection():
 
 
 # def add_subscriber(con, email):
-#     time_now = datetime.now()
-#     half_of_year = timedelta(days=182)
-#     ending_time = time_now + half_of_year
-#     print(ending_time)
-#     sql_insert_query = """INSERT INTO 'sqlitedb_subscribers'
-#                               ('email', 'ending_date')
-#                               VALUES (?, ?);"""
-#     cursor = con.cursor()
-#     cursor.execute(sql_insert_query, (email, ending_time))
-#     con.commit()
-#     cursor.close()
+#     try:
+#         time_now = datetime.now().date()
+#         half_of_year = timedelta(days=182)
+#         ending_time = time_now + half_of_year
+#         sql_insert_query = """INSERT INTO 'sqlitedb_subscribers'
+#                                   ('email', 'ending_date')
+#                                   VALUES (?, ?);"""
+#         cursor = con.cursor()
+#         cursor.execute(sql_insert_query, (email, ending_time))
+#         con.commit()
+#         cursor.close()
+#     except Error:
+#         print('такой уже есть')
 #
 #
 # sqlite_connection = sql_connection()
@@ -61,12 +79,12 @@ def sql_connection():
 
 
 # def check_subscribtion(con, mail):
-#     time_now = datetime.now()
+#     time_now = datetime.now().date()
 #     sqlite_select_query = """SELECT * from sqlitedb_subscribers where email = ?"""
 #     cursor = con.cursor()
 #     cursor.execute(sqlite_select_query, (mail,))
 #     record = cursor.fetchone()
-#     time = datetime.strptime(record[1], '%m/%d/%y %H:%M:%S')
+#     time = date(*list(map(int, record[1].split('-'))))
 #     if time > time_now:
 #         con.commit()
 #         cursor.close()
@@ -74,8 +92,8 @@ def sql_connection():
 #     con.commit()
 #     cursor.close()
 #     return False
-# 
-# 
+#
+#
 # sqlite_connection = sql_connection()
 # mail = input()
 # print(check_subscribtion(sqlite_connection, mail))
