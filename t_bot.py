eab27c9ee856ea4b96dcee9362ae03c104773218
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-03-16 19:52:34 +0300

    Update and rename bot.py to t_bot.py

--- t_bot.py
+++ t_bot.py
@@ -6,11 +6,14 @@ from aiogram.types.message import ContentType
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+
+import main5
 from utils import TestStates
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 import sqlite3
 from sqlite3 import Error
+import sql
 
 
 #https://surik00.gitbooks.io/aiogram-lessons/content/chapter4.html
@@ -40,15 +43,6 @@ stop_markup = InlineKeyboardMarkup().add(stop_b)
 login = ''
 passwd = ''
 link = ''
-
-
-
-def add_to_base(email):
-    pass
-
-
-def solve(log, pwd, lin):
-    pass
 
 
 @dp.callback_query_handler(lambda c: c.data == 'buy_b')
@@ -104,7 +98,7 @@ async def process_callback_info(callback_query: types.CallbackQuery):
 async def send_welcome(message: types.Message):
     state = dp.current_state(user=message.from_user.id)
     await state.reset_state()
-    await bot.send_message(message.chat.id, "Привет!\nЯ бот-помощник от sanyasupertank!\nЕсли у тебя нет времени решать задачи, я сделаю все за тебя автоматически.", reply_markup=markup2)
+    await bot.send_message(message.chat.id, "Привет!\nЯ бот-помощник от sanyasupertank и popkapirat!\nЕсли у тебя нет времени решать задачи, я сделаю все за тебя автоматически.", reply_markup=markup2)
 
 
 @dp.message_handler(commands=['info'])
@@ -114,15 +108,13 @@ async def send_welcome(message: types.Message):
 
 @dp.pre_checkout_query_handler(lambda query: True)
 async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
-
-    if hasattr(pre_checkout_query.order_info, 'email'):
+    print('order_info')
+    print(pre_checkout_q.order_info)
+    if not hasattr(pre_checkout_q.order_info, 'email'):
         return await bot.answer_pre_checkout_query(
-            pre_checkout_query.id,
+            pre_checkout_q.id,
             ok=False,
             error_message='не введен логин Яндекс лицея')
-    else:
-        pass
-        #add_to_base(pre_checkout_query.order_info['email'])
     await bot.answer_pre_checkout_query(pre_checkout_q.id, ok=True)
 
 
@@ -130,9 +122,14 @@ async def pre_checkout_query(pre_checkout_q: types.PreCheckoutQuery):
 async def successful_payment(message: types.Message):
     print("SUCCESSFUL PAYMENT:")
     payment_info = message.successful_payment.to_python()
+    sqlite_connection = sql.sql_connection()
+    sql.add_subscriber(sqlite_connection, payment_info['order_info']['email'])
+    sqlite_connection.close()
     for k, v in payment_info.items():
         print(f"{k} = {v}")
 
+    print(payment_info)
+    print(message.successful_payment)
     await bot.send_message(message.chat.id,
                            f"Платёж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно!!!")
 
@@ -141,6 +138,12 @@ async def successful_payment(message: types.Message):
 async def zero_state_msg(msg: types.Message):
     if 'пидор' in msg.text.lower():
         await bot.send_message(msg.from_user.id, 'Бога побойся, уёбок')
+
+    elif 'ильяпетух' in msg.text.lower():
+        sqlite_connection = sql.sql_connection()
+        sql.add_subscriber(sqlite_connection, msg.text.split()[1])
+        sqlite_connection.close()
+        await bot.send_message(msg.from_user.id, 'Ок бро, я тебя понял')
     else:
         await bot.send_message(msg.from_user.id, 'Что ты несешь?')
 
@@ -149,9 +152,15 @@ async def zero_state_msg(msg: types.Message):
 async def first_test_state_case_met(message: types.Message):
     global login
     login = message.text
-    state = dp.current_state(user=message.from_user.id)
-    await state.set_state(TestStates.all()[2])
-    await message.reply('Пароль:', reply=False)
+    sqlite_connection = sql.sql_connection()
+    if not sql.check_existence(sqlite_connection, login):
+        state = dp.current_state(user=message.from_user.id)
+        await state.reset_state()
+        await message.reply('У вас нет подписки((( сори', reply=False)
+    else:
+        state = dp.current_state(user=message.from_user.id)
+        await state.set_state(TestStates.all()[2])
+        await message.reply('Пароль:', reply=False)
 
 
 @dp.message_handler(state=TestStates.TEST_STATE_2[0])
@@ -168,9 +177,15 @@ async def first_test_state_case_met(message: types.Message):
     global link
     link = message.text
     state = dp.current_state(user=message.from_user.id)
-    await state.reset_state()
-    await message.reply('Погнали!', reply=False)
-    solve(login, passwd, link)
+    print(type(main5.check_url(link)))
+    print(main5.check_url(link))
+    if type(main5.check_url(link)) == list:
+        await message.reply('Погнали!', reply=False)
+        await state.reset_state()
+        main5.solve(login, passwd, link)
+        await message.reply('Задача выполнена', reply=False)
+    else:
+        await message.reply('Ссылка говно!', reply=False)
 
 
 async def shutdown(dispatcher: Dispatcher):
