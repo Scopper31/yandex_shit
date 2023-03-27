c5b471404f988877ffdb4e6cec4e97c8a754be9a
pipapipapa <113211549+pipapipapa@users.noreply.github.com>
2023-03-28 00:59:44 +0300

    Update cringe.py

--- cringe.py
+++ cringe.py
@@ -281,13 +281,13 @@ def check_url(url):
     url = url.split('/')
     domain = url[0]
     if domain == "lyceum.yandex.ru":
-        if len(url) >= 1:
+        if len(url) > 1:
             if 'courses' == url[1]:
-                if len(url) >= 3:
+                if len(url) > 3:
                     if 'groups' == url[3]:
-                        if len(url) >= 5:
+                        if len(url) > 5:
                             if 'lessons' == url[5]:
-                                if len(url) >= 7:
+                                if len(url) > 7:
                                     if 'tasks' == url[7]:
                                         return ['task']
                                     else:
