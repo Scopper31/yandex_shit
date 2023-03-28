f18dd0a1b0ca1c229002a5c683ccd7082a34f579
pipapipapa <113211549+pipapipapa@users.noreply.github.com>
2023-03-28 09:48:48 +0300

    Update cringe.py

--- cringe.py
+++ cringe.py
@@ -57,7 +57,7 @@ num_markup = InlineKeyboardMarkup().add(yes_b).add(no_b)
 
 
 class User:
-    def __init__(self, login='', wanna_commit_suicide='', driver='', qr_code='', send_time=datetime.datetime.now()):
+    def __init__(self, login='', wanna_commit_suicide='', driver='', qr_code='', send_time=datetime.datetime(2035, 1, 1, 1, 1)):
         self.login = login
         self.links = []
         self.driver = driver
