f7e2d3ac97b4b6cf1577a07e684e55e13a4a6e45
pipapipapa <113211549+pipapipapa@users.noreply.github.com>
2023-03-29 23:36:29 +0300

    Update cringe.py

--- cringe.py
+++ cringe.py
@@ -25,7 +25,7 @@ from utils import TestStates
 
 openai.api_key = key
 
-template = 'Python, dont write any comments, provide answer in code block\nThe problem: '
+template = 'Python, dont write any comments, provide answer in code block, obey pep8\nThe problem: '
 sample_template = ["\nFor this example:\n", "\nIt outputs this:\n", "\nFor example if program gets this input:\n"]
 funcclass_template = ["\nAn example of program that might use your code:\n",
                       "\nOutput it needs to produce:\n"]
